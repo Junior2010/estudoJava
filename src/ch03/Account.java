9cd5f2e468fc467c961e8a216049ec883590b3aa
ildef <ildef@DESKTOP-42GAIBN>
2020-04-25 05:33:50 -0300

    Capítulo03-01

---
+++ src/ch03/Account.java
@@ -0,0 +1,20 @@
+package ch03;
+//
+//
+
+public class Account
+{
+	private String name;
+	
+	//método para definir o nome do objeto
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	//métodp ára recuperar o nome do objeto
+	public String getName()
+	{
+		return name;
+	}
+}
